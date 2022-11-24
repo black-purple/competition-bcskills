72cbbd9298f8d10cf36833055cf7828880bd90c6
Abderrahman DAKIR ALLAH <dakirallah.ar@gmail.com>
2022-11-24 13:16:14 +0100

    dashbaord updates

--- frontend/views/dashboard.php
+++ frontend/views/dashboard.php
@@ -1,11 +1,18 @@
 <!DOCTYPE html>
 <html lang="en">
 <?php
-    // session_start();
-    // if (!$_SESSION['currentUser']) {
-    //     header("Location:./login.php?login");
-    // }
+session_start();
+include("../../backend/admin.php");
+include("../../backend/patient.php");
+if (!$_SESSION['currentUser']) {
+    header("Location:./login.php?login");
+}
+if (isset($_POST['logout'])) {
+    Admin::logout();
+    header("Location: ./login.php");
+}
 ?>
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -23,11 +30,14 @@
                 <div class="profile">
                     <div class="profile_imgname">
                         <div class="profile_img"></div>
-                        <p><?php echo "User";// echo $_SESSION['currentUser']['email'] || 'User'?></p>
+                        <p><?php echo $_SESSION['currentUser']['email']; ?></p>
                     </div>
                     <div class="logout">
-                        <form method="post"></form>
-                        <i class="fa-solid fa-arrow-right-from-bracket"></i>
+                        <form method="post">
+                            <button type="submit" class="logout-btn" name="logout">
+                                <i class="fa-solid fa-arrow-right-from-bracket"></i>
+                            </button>
+                        </form>
                     </div>
                 </div>
                 <div class="menu">
@@ -44,52 +54,53 @@
                 <div class="dashboard_body_nav">
                     <div class="body_nav">
                         <div class="icons"></div>
-                        <div class="title"> <span>Dashbords / </span> Dossier </div>
+                        <div class="title"> <span>Dashbord / </span> Dossier </div>
                     </div>
                 </div>
                 <div class="body_wrapper">
-                    <div class="dashboard_body_body">                    
-                    <!--clients infos-->
-                    <div class="info_table">
-                        <table>
-                            
-                            <thead>
-                              <tr>
-                                <th scope="col">CIN</th>
-                                <th scope="col">nom complet</th>
-                                <th scope="col">sex</th>
-                              </tr>
-                              <tr class="border-bottom"></tr>
-                            </thead>
-                            <tbody>
-                              <tr>
-                                <td>xxxx</td>
-                                <td>1976</td>
-                                <td>9</td>
-                              </tr>
-                              <tr>
-                                <td>xxxx</td>
-                                <td>1976</td>
-                                <td>6</td>
-                              </tr>
-                          
-                              <!-- several other great bands -->
-                          
-                              <tr>
-                                <td>xxxx</td>
-                                <td>1974</td>
-                                <td>17</td>
-                              </tr>
-                            </tbody>
-                            
-                          </table>
-                          
-                    </div>
+                    <div class="dashboard_body_body">
+                        <!--clients infos-->
+                        <div class="info_table">
+                            <table>
+
+                                <thead>
+                                    <tr>
+                                        <th scope="col">CIN</th>
+                                        <th scope="col">nom complet</th>
+                                        <th scope="col">sex</th>
+                                    </tr>
+                                    <tr class="border-bottom"></tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>xxxx</td>
+                                        <td>1976</td>
+                                        <td>9</td>
+                                    </tr>
+                                    <tr>
+                                        <td>xxxx</td>
+                                        <td>1976</td>
+                                        <td>6</td>
+                                    </tr>
+
+                                    <!-- several other great bands -->
+
+                                    <tr>
+                                        <td>xxxx</td>
+                                        <td>1974</td>
+                                        <td>17</td>
+                                    </tr>
+                                </tbody>
+
+                            </table>
+
+                        </div>
                     </div>
                 </div>
-            </div>              
+            </div>
         </div>
     </div>
 
 </body>
+
 </html>
