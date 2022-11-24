38b8ad813b44f296641a5d57d5f117492b3b9fa9
Abderrahman DAKIR ALLAH <dakirallah.ar@gmail.com>
2022-11-24 11:12:45 +0100

    traitement code

--- backend/models/traitement.php
+++ backend/models/traitement.php
@@ -1,5 +1,25 @@
 <?php
 class Traitement {
+    static public function connexion() {
+        try {
+            $servername = "localhost";
+            $conn = new PDO("mysql:host=$servername;dbname=bcskills", "root", "");
+            return $conn;
+        } catch (PDOException $e) {
+            return null;
+        }
+    }
+
+    static public function getTraitement($numTraitement) {
+        Patient::connexion()->exec("SELECT * FROM traitement WHERE numTraitement='$numTraitement'");
+    }
     
+    static public function getPatientTraitements($numDossierPatient) {
+        Patient::connexion()->exec("SELECT * FROM traitement WHERE numDossier='$numDossierPatient'");
+    }
+
+    static public function addTraitement($numDossier) {
+        Patient::connexion()->exec("INSERT INTO traitement(nomMedecin, dateRencontre,detailsRencontre,conclusion,decision) WHERE numDossier='$numDossier'");
+    }
+
 }
-?>
