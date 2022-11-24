6c22c7d84d03fc90821053744ab52e4e01493b58
Abderrahman DAKIR ALLAH <dakirallah.ar@gmail.com>
2022-11-24 11:05:50 +0100

    patient code

--- backend/models/patient.php
+++ backend/models/patient.php
@@ -10,15 +10,26 @@ class Patient {
         }
     }
 
+    static public function getPatient($numDossier) {
+        
+        Patient::connexion()->exec("SELECT * FROM patient WHERE numDossier='$numDossier'");
+    }
+    
+    static public function getAllPatient() {
+        
+        Patient::connexion()->exec("SELECT * FROM patient");
+    }
+
     static public function addPatient($numDossier, $cin, $nomComplet, $dateNaissance, $sexe, $profession, $tel, $adresse, $numSecSociale,$payeurs,$mutuelle, $dAlertes) {
         Patient::connexion()->exec("INSERT INTO patient()");
     }
 
     static public function editPatient($numDossier, $cin, $nomComplet, $dateNaissance, $sexe, $profession, $tel, $adresse, $numSecSociale,$payeurs,$mutuelle, $dAlertes){
-        Patient::connexion()->exec("UPDATE patient SET ");
+        Patient::connexion()->exec("UPDATE patient SET numDossier='$numDossier', cin='$cin', nomComplet='$nomComplet', dateNaissance='$dateNaissance', sexe='$sexe', profession='$profession', tel='$tel', adresse='$adresse', numSecSociale='$numSecSociale', mutuelle='$mutuelle', payeurs='$payeurs', dAlertes='$dAlertes'");
     }
 
     static public function archivePatient($numDossier) {
-        
+        Patient::connexion()->exec("UPDATE patient SET archive='1' WHERE numDossier='$numDossier'");
     }
+
 }
